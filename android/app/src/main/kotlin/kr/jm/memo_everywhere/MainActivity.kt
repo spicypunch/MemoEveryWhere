a99af37acb180b93027071e0c332195c85e1ebbe
spicypunch <miniguni66@naver.com>
2025-01-06 12:14:25 +0900

    first commit

---
+++ android/app/src/main/kotlin/kr/jm/memo_everywhere/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.jm.memo_everywhere
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('종마루'),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
