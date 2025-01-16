3a3fca507024796fd8dc9af38ffe0932425d69b9
KJM-BlueVisor <kjm@bluevisor.kr>
2025-01-16 12:02:17 +0900

    feature: SupaBase 적용

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memo_everywhere/shared/provider_observer.dart';
 import 'package:memo_everywhere/shared/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(
-      ProviderScope(observers: [Logger()], child: _App())
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Future<void> main() async {
+    await Supabase.initialize(
+      url: 'https://swwywvcpsvrmpggtecen.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3d3l3dmNwc3ZybXBnZ3RlY2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5OTUwODgsImV4cCI6MjA1MjU3MTA4OH0.1z5eMq4pkBibYLw31pE8-ebv6f3nwxArZmMXWU2Hvbc',
+    );
+    runApp(
+        ProviderScope(observers: [Logger()], child: _App())
+    );
+  }
 }
 
 class _App extends ConsumerWidget {
