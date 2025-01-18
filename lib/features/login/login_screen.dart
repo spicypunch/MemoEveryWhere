a4676015ac127730f3b902936af1b64c1a4d2605
spicypunch <miniguni66@naver.com>
2025-01-18 23:54:27 +0900

    feature: 로그인 구현

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_everywhere/shared/components/default_button.dart';
 import 'package:memo_everywhere/shared/components/default_layout.dart';
+import 'package:memo_everywhere/shared/components/default_text_field.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
@@ -14,11 +16,32 @@ class LoginScreen extends ConsumerWidget {
             horizontal: 20.0,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 '로그인',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
+              DefaultTextFormField(label: 'email'),
+              Text(
+                '비밀번호',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              DefaultTextFormField(label: 'password'),
+              DefaultButton(title: '로그인', onTap: () {}),
+              Center(
+                child: InkWell(
+                  onTap: () {},
+                  child: Text(
+                    '회원가입',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- lib/shared/go_router.dart
+++ lib/shared/go_router.dart
@@ -11,7 +11,7 @@ import '../features/main/main_screen.dart';
 final routerProvider = Provider<GoRouter>(
   (ref) {
     return GoRouter(
-      initialLocation: '/splash',
+      initialLocation: '/login',
       redirect: (BuildContext context, GoRouterState state) {
         return null;
       },
