825a45015f2a38c9e0801f3f75a6624cc1e7cae9
spicypunch <miniguni66@naver.com>
2025-01-27 23:40:21 +0900

    feature: Repository 생성

--- lib/data/repository_impl.dart
+++ lib/data/repository_impl.dart
@@ -4,7 +4,7 @@ import 'package:memo_everywhere/domain/repository.dart';
 class RepositoryImpl implements Repository {
   @override
   Future<AuthUser> signIn(String email, String passWord) {
-    // TODO: implement signIn
+
     throw UnimplementedError();
   }
 
