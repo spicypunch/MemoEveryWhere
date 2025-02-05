7d5a8618890977a303c19f8545b208a3b8d51718
spicypunch <miniguni66@naver.com>
2025-02-05 23:34:30 +0900

    feature: firebase로 변경

--- lib/data/repository_impl.dart
+++ lib/data/repository_impl.dart
@@ -1,50 +1,50 @@
-import 'package:memo_everywhere/domain/repository.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-class RepositoryImpl implements Repository {
-  final SupabaseClient _client;
-
-  RepositoryImpl(this._client);
-
-  @override
-  Future<bool> signIn(String email, String passWord) async {
-    try {
-      final response = await _client.auth.signInWithPassword(
-        email: email,
-        password: passWord,
-      );
-
-      if (response.user == null) {
-        throw Exception('로그인 실패');
-      }
-      return response.user != null;
-    } catch (e) {
-      throw Exception('로그인 실패: ${e.toString()}');
-    }
-  }
-
-  @override
-  Future<bool> signup(String email, String passWord) async {
-    try {
-      final response = await _client.auth.signUp(
-        email: email,
-        password: passWord,
-        emailRedirectTo: null,
-      );
-      return response.user != null;
-    } catch (e) {
-      throw Exception('회원가입 실패: ${e.toString()}');
-    }
-  }
-
-  @override
-  Future<bool> signOut() async {
-    try {
-      final response = await _client.auth.signOut();
-      return true;
-    } catch (e) {
-      throw Exception('로그아웃 실패: ${e.toString()}');
-      return false;
-    }
-  }
-}
+// import 'package:memo_everywhere/domain/repository.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
+//
+// class RepositoryImpl implements Repository {
+//   final SupabaseClient _client;
+//
+//   RepositoryImpl(this._client);
+//
+//   @override
+//   Future<bool> signIn(String email, String passWord) async {
+//     try {
+//       final response = await _client.auth.signInWithPassword(
+//         email: email,
+//         password: passWord,
+//       );
+//
+//       if (response.user == null) {
+//         throw Exception('로그인 실패');
+//       }
+//       return response.user != null;
+//     } catch (e) {
+//       throw Exception('로그인 실패: ${e.toString()}');
+//     }
+//   }
+//
+//   @override
+//   Future<bool> signup(String email, String passWord) async {
+//     try {
+//       final response = await _client.auth.signUp(
+//         email: email,
+//         password: passWord,
+//         emailRedirectTo: null,
+//       );
+//       return response.user != null;
+//     } catch (e) {
+//       throw Exception('회원가입 실패: ${e.toString()}');
+//     }
+//   }
+//
+//   @override
+//   Future<bool> signOut() async {
+//     try {
+//       final response = await _client.auth.signOut();
+//       return true;
+//     } catch (e) {
+//       throw Exception('로그아웃 실패: ${e.toString()}');
+//       return false;
+//     }
+//   }
+// }
